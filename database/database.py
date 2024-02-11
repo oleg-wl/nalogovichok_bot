bc8f5c93b8f0fe9f6c1334d55f2ddea58092e964
oleg_vl <@nitro-pc>
2024-02-11 18:18:04 +0300

    Подключение к БД Clickhouse
    В модуле database добавлен класс Client для подключения к ДБ

--- database/database.py
+++ database/database.py
@@ -1,29 +1,14 @@
 import os
 
-from sqlalchemy import create_engine, text
+import clickhouse_connect 
 
-# Укажите параметры подключения к вашей базе данных PostgreSQL
-db_user = os.environ['db_user']
-db_password = os.environ['db_password']
-db_host = os.environ['db_host']
-db_port = int(os.environ['db_port'])
-db_name = os.environ['db_name']
 
-# Формируем строку подключения
-connection_string = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+class Client:
 
-# Создаем объект Engine для подключения к базе данных
-engine = create_engine(connection_string)
+    def __init__(self):
+        self.client = clickhouse_connect.get_client(host='localhost', port=18123, username='default')
 
-# Подключаемся к базе данных
-connection = engine.connect()
+    def test(self):
+        result = self.client.query("SELECT 'HELLO WORLD'")
+        print(result.result_rows)
 
-# Выполняем SQL-запрос
-result = connection.execute(text('SELECT version()'))
-
-# Выводим результат запроса
-for row in result:
-    print(row)
-
-# Закрываем соединение
-connection.close()
